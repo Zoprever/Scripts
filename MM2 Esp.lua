3b932a309a294ce04d9cf81d609ec2e397b44bbf
Zoprever <zoprever@gmail.com>
2025-12-17 20:11:02 -0700

    Add loading of Sheriff Esp script

--- MM2 Esp.lua
+++ MM2 Esp.lua
@@ -1,2 +1,3 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Zoprever/Scripts/refs/heads/main/Trash/Murder%20Esp.lua"))()
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Zoprever/Scripts/refs/heads/main/Trash/Sheriff%20Esp.lua"))()
