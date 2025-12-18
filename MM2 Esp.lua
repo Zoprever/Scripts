e9c4afb36e706c74ffbe17e785ce437181a2e7a0
Zoprever <zoprever@gmail.com>
2025-12-17 19:54:52 -0700

    Fix loadstring calls for MM2 Esp.lua

--- MM2 Esp.lua
+++ MM2 Esp.lua
@@ -1,2 +1,2 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Zoprever/Scripts/refs/heads/main/Trash/Murder%20Esp.lua"))
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Zoprever/Scripts/refs/heads/main/Trash/Sheriff%20Esp.lua"))
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Zoprever/Scripts/refs/heads/main/Trash/Murder%20Esp.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Zoprever/Scripts/refs/heads/main/Trash/Sheriff%20Esp.lua"))()
