8139f6b8d0689b134dc2b476e1cf227cba68c913
Zoprever <zoprever@gmail.com>
2026-01-31 07:39:09 -0700

    Add ImageButton with custom size and image
    Added an ImageButton with specific size and properties.

--- MAP/test.lua
+++ MAP/test.lua
@@ -1,4 +1,9 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Zoprever/RLibrarys/refs/heads/main/Zorex/ZorexUI.lua"))()
 local ScreenGui = SearchForUI("ZorexUI")
 local IB = Instance.new("ImageButton")
-IB.Size = UDim2.new()
+IB.Size = UDim2.new(0, 50, 0, 50)
+IB.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+IB.Image = getcustomasset("ZorexSystem/Assets/Execute.png")
+IB.Parent = ScreenGui.Window
+local UC = Instance.new("UICorner")
+UC.CornerRadius = UDim.new(1, 0)
