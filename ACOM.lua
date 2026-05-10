b649f173ba4af318b5ce249902095d4185744c8c
Zoprever <zoprever@gmail.com>
2026-05-09 19:24:09 -0700

    Update ACOM creation to use Part instance
    Change ACOM instance creation to use Part instead of Ball.

--- ACOM.lua
+++ ACOM.lua
@@ -1,6 +1,12 @@
 -- Variables
 local Player = game:GetService("Players").LocalPLayer
 local Char = Player.Character or Player.CharacterAdded:Wait()
+
+-- ACOM
 local function CreateAcom()
-  local ACOM = Instance.new("Ball")
+  local ACOM = Instance.new("Part", workspace)
+  ACOM.Name = "ACOM"
+  ACOM.Shape = "Ball"
+  ACOM.Size = Vector3.new(2, 2, 2)
+  
 end
