86e1ecaf6331eab167752dcf954523b769707d66
Zoprever <zoprever@gmail.com>
2026-05-09 19:06:18 -0700

    Update ACOM.lua

--- ACOM.lua
+++ ACOM.lua
@@ -1,3 +1,5 @@
 -- Variables
 local Player = game:GetService("Players").LocalPLayer
 local Char = Player.Character or Player.CharacterAdded:Wait()
+local function CreateAcom()
+  local ACOM = Instance.new("")
