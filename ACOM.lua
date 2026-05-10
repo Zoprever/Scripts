6e912d204ca72915817075c39fbfc15f8ac03d09
Zoprever <zoprever@gmail.com>
2026-05-09 19:06:39 -0700

    Fix ACOM instance creation to use 'Ball' type

--- ACOM.lua
+++ ACOM.lua
@@ -2,4 +2,5 @@
 local Player = game:GetService("Players").LocalPLayer
 local Char = Player.Character or Player.CharacterAdded:Wait()
 local function CreateAcom()
-  local ACOM = Instance.new("")
+  local ACOM = Instance.new("Ball")
+end
